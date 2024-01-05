ad53013b969547fe717faf5cc68ee323d8b99fcc
Rahul <rahulvish194002@gmial.com>
2024-01-06 01:46:26 +0530

    cake shop management

---
+++ src/main/java/com/mycompany/cakeshopmanagement/Cakeshopmanagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cakeshopmanagement;
+
+/**
+ *
+ * @author Viraj
+ */
+public class Cakeshopmanagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
